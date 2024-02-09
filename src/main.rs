d9f1a108c153e0b6e669a483388ac07c90d0b98f
jakub-figat <jakub.figat@neontri.com>
2024-02-09 12:03:31 +0100

    Add simple test

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,30 @@ fn main() {
 
     println!("Search time: {} microseconds", search.elapsed().as_micros());
 }
+
+#[cfg(test)]
+mod tests {
+    use rand::prelude::SliceRandom;
+    use rand::thread_rng;
+    use storage::SkipList;
+
+    #[test]
+    fn test_list() {
+        {
+            let mut list = SkipList::new(16, 0.5);
+
+            let mut rng = thread_rng();
+            let mut nums: Vec<_> = (1..=i32::pow(10, 3)).collect();
+            nums.shuffle(&mut rng);
+
+            for num in nums.iter() {
+                list.insert(num.clone(), num.clone()).unwrap();
+            }
+
+            list.get(&777).unwrap();
+            list.delete(&777).unwrap();
+            list.insert(777, 1).unwrap();
+            list.get(&777).unwrap();
+        }
+    }
+}
